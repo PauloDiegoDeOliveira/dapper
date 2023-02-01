ed9aab0eb8d031bdc74c6ee4687054729918b8ff
Paulo Diego <paulo.diego@adm.unip.br>
2023-02-01 12:09:18 -0300

    Referencias

---
+++ Empresa.Dapper.Domain/Entitys/Participante.cs
@@ -0,0 +1,13 @@
+﻿using Empresa.Dapper.Domain.Entitys.Base;
+
+namespace Empresa.Dapper.Domain.Entitys
+{
+    public class Participante : EntityBase
+    {
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string CPF { get; set; }
+    }
+}
---
+++ Empresa.Dapper.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Empresa.Dapper.Domain.Entitys;
+using Microsoft.EntityFrameworkCore;
+
+namespace Empresa.Dapper.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Participante> Participante { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            foreach (var entry in ChangeTracker.Entries())
+            {
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("AlteradoEm").CurrentValue = DateTime.Now;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
