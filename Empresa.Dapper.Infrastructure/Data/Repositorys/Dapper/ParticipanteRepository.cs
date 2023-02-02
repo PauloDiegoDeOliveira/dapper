cf4ad5607d55bfaa2b1d8a54479ce49f15d220d9
Paulo Diego <paulo.diego@unip.br>
2023-02-02 09:27:33 -0300

    OpenAsync

--- Empresa.Dapper.Infrastructure/Data/Repositorys/Dapper/ParticipanteRepository.cs
+++ Empresa.Dapper.Infrastructure/Data/Repositorys/Dapper/ParticipanteRepository.cs
@@ -27,45 +27,58 @@ namespace Empresa.Dapper.Infrastructure.Data.Repositorys.Dapper
 
         public async Task<PagedList<Participante>> GetPaginationAsync(ParametersPalavraChave parametersPalavraChave)
         {
-            using (conexaoSql)
+            try
             {
-                IQueryable<Participante> participantes = (await conexaoSql.QueryAsync<Participante>(ParticipanteScript.GetAll)).AsQueryable();
+                await conexaoSql.OpenAsync();
 
-                if (parametersPalavraChave.PalavraChave is null && parametersPalavraChave.Id is null && parametersPalavraChave.Status is 0)
-                    participantes = participantes.Where(participante => participante.Status != EStatus.Excluido.ToString());
-                else if (parametersPalavraChave.Status != 0)
-                    participantes = participantes.Where(participante => participante.Status == parametersPalavraChave.Status.ToString());
-
-                if (parametersPalavraChave.Id is not null)
-                    participantes = participantes.Where(participante => parametersPalavraChave.Id.Contains(participante.Id));
-
-                // TODO: Verificar erro com like
-                if (!string.IsNullOrEmpty(parametersPalavraChave.PalavraChave))
-                    participantes = participantes.Where(participante => EF.Functions.Like(participante.Nome, $"%{parametersPalavraChave.PalavraChave}%"));
-
-                if (parametersPalavraChave.Ordenar != 0)
+                using (conexaoSql)
                 {
-                    switch (parametersPalavraChave.Ordenar.ToString())
+                    IQueryable<Participante> participantes = (await conexaoSql.QueryAsync<Participante>(ParticipanteScript.GetAll)).AsQueryable();
+
+                    if (parametersPalavraChave.PalavraChave is null && parametersPalavraChave.Id is null && parametersPalavraChave.Status is 0)
+                        participantes = participantes.Where(participante => participante.Status != EStatus.Excluido.ToString());
+                    else if (parametersPalavraChave.Status != 0)
+                        participantes = participantes.Where(participante => participante.Status == parametersPalavraChave.Status.ToString());
+
+                    if (parametersPalavraChave.Id is not null)
+                        participantes = participantes.Where(participante => parametersPalavraChave.Id.Contains(participante.Id));
+
+                    // TODO: Verificar erro com like
+                    if (!string.IsNullOrEmpty(parametersPalavraChave.PalavraChave))
+                        participantes = participantes.Where(participante => EF.Functions.Like(participante.Nome, $"%{parametersPalavraChave.PalavraChave}%"));
+
+                    if (parametersPalavraChave.Ordenar != 0)
                     {
-                        case "Crescente":
-                            participantes = participantes.OrderBy(participante => participante.Nome);
-                            break;
+                        switch (parametersPalavraChave.Ordenar.ToString())
+                        {
+                            case "Crescente":
+                                participantes = participantes.OrderBy(participante => participante.Nome);
+                                break;
 
-                        case "Decrescente":
-                            participantes = participantes.OrderByDescending(participante => participante.Nome);
-                            break;
+                            case "Decrescente":
+                                participantes = participantes.OrderByDescending(participante => participante.Nome);
+                                break;
 
-                        case "Novos":
-                            participantes = participantes.OrderByDescending(participante => participante.CriadoEm);
-                            break;
+                            case "Novos":
+                                participantes = participantes.OrderByDescending(participante => participante.CriadoEm);
+                                break;
 
-                        case "Antigos":
-                            participantes = participantes.OrderBy(participante => participante.CriadoEm);
-                            break;
+                            case "Antigos":
+                                participantes = participantes.OrderBy(participante => participante.CriadoEm);
+                                break;
+                        }
                     }
-                }
 
-                return await Task.FromResult(PagedList<Participante>.ToPagedList(participantes, parametersPalavraChave.NumeroPagina, parametersPalavraChave.ResultadosExibidos));
+                    return await Task.FromResult(PagedList<Participante>.ToPagedList(participantes, parametersPalavraChave.NumeroPagina, parametersPalavraChave.ResultadosExibidos));
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                await conexaoSql.CloseAsync();
             }
         }
     }
